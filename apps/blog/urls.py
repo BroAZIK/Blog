652dcea4474ba24dd1327355adfce751014ab17c
BroAZIK <tolliboyevazizbek21@gmail.com>
2024-10-27 11:25:57 +0500

    done

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import BloggDetailView, BlogListView
+from .views import *
 
 urlpatterns = [
-    path('', BlogListView.as_view(), name='blog')
+    path('post/new/', BlogCreateView.as_view(), name='new_post'),
+    path('', BlogListView.as_view(), name='blog'),
+    path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail')
 ]
